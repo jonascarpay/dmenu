f9d4cddcb48a1475e44cde1cc5b5741aa6d70c0e
Jonas Carpay <jonascarpay@gmail.com>
2022-06-06 11:19:42 +0900

    Styling defaults

--- config.def.h
+++ config.def.h
@@ -9,12 +9,12 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#eceff4", "#2e3440" },
+	[SchemeSel] = { "#2e3440", "#81a1c1" },
+	[SchemeOut] = { "#2e3440", "#5e81ac" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 10;
 
 /*
  * Characters not considered part of a word while deleting words
